9f4e646526e362eeda5b33cf27ccf8863c5ce386
HKGP\linziy <linziy@huntkey.net>
2018-01-04 17:54:22 +0800

    java 学习 ，测试项目

---
+++ src/main/java/study/Test.java
@@ -0,0 +1,10 @@
+package study;
+
+/**
+ * Created by linziy on 2018/1/4.
+ */
+public class Test {
+    public final static void main(String[] args){
+        System.out.println("这是个测试入口");
+    }
+}
